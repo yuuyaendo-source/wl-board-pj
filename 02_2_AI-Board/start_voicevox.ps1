086d87d588aa19b4b9d044fb5ea70c3f39918a84
hisashiasakawa <hisashi.asakawa@wonder-link.co.jp>
2026-02-21 01:43:09 +0900

    ディレクトリ構成変更

--- 02_2_AI-Board/start_voicevox.ps1
+++
@@ -1,7 +0,0 @@
-# VOICEVOX Engine Docker Container Startup Script (GPU)
-# Requirement: Docker Desktop & NVIDIA GPU
-
-write-host "Starting VOICEVOX Engine (GPU Mode)..."
-docker run --rm -it -p 50021:50021 --gpus all voicevox/voicevox_engine:nvidia-ubuntu20.04-latest
-
-# Note: The first run will take time to download the image.
